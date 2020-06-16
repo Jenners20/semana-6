548815a2b7270e1ef10e52620d5309f1d567dbdc
Jenners20 <61439419+Jenners20@users.noreply.github.com>
2020-06-15 22:10:54 -0400

    Add files via upload

---
+++ Ejercicio6/Ejercicio6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*6. Recorrer los números del 1 al 50 en un ciclo for */
+
+            for (int i = 1; i < 51; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
