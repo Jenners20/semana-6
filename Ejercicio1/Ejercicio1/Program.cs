a53e99670e9e7faf252f1a69c890594355d992a2
Jenners20 <61439419+Jenners20@users.noreply.github.com>
2020-06-15 21:57:15 -0400

    Add files via upload

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1. Crear una aplicación que muestre la tabla de multiplicar del 7, tips usar ciclo while.*/
+
+            int limite = 0;
+            int factor = 0;
+            int contador = 0;
+            int producto = 0;
+
+            Console.Write("Ingrese el límite para la tabla de multiplicar del 7: ");
+            limite = int.Parse(Console.ReadLine());
+
+            while (factor< limite)
+            {
+                factor++;
+                producto = 7 * factor;
+                Console.WriteLine(" 7 x {0} = {1}",factor, producto);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
