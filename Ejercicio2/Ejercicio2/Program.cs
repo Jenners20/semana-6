da0e6d993b618b22ba8ef38e43ffe4e439dd68ec
Jenners20 <61439419+Jenners20@users.noreply.github.com>
2020-06-15 22:00:44 -0400

    Add files via upload

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2. Crear una aplicación que le pida al usuario un numero
+             * positivos y el sistema identifique si es primo o no */
+
+            int numero = 0;
+            int c = 0;
+
+            Console.Write("Ingrese un numero entero positivo: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < (numero+1); i++)
+            {
+                if (numero%i==0)
+                {
+                    c++;
+                }
+            }
+            if (c !=2)
+            {
+                Console.WriteLine("El numero {0} no es primo ",numero);
+            }
+            else
+            {
+                Console.WriteLine("El numero {0} es primo ", numero);
+            }
+            Console.ReadKey();
+        }
+    }
+}
